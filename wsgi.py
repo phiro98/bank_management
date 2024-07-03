d4fad76cf312cabd496e7069eaa9d00222c9f145
phiro98 <phiroze.pc14@gmail.com>
2024-07-03 11:06:03 +0530

    vercell conf removed

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-#set the path
-import os, sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port = 5000)
